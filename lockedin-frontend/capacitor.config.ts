f5919cc9bbb5b6775d56124515f4acd96c59492f
Nguyen Dinh Phuc - SE181962 <phucndse181962@fpt.edu.vn>
2026-06-26 14:24:05 +0700

    Revert "Thêm toàn bộ thư mục Frontend và giao diện HLV"

--- lockedin-frontend/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.lockedin.app',
-  appName: 'LockedIn',
-  webDir: 'dist'
-};
-
-export default config;
