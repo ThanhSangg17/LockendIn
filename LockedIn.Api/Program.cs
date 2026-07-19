a5a14dfe49b44a4e7a7057aa996351a0c857a8ec
Nguyen Thanh Sang <sangntse180064@fpt.edu.vn>
2026-07-19 13:33:59 +0700

    fix: enable Swagger in production

--- LockedIn.Api/Program.cs
+++ LockedIn.Api/Program.cs
@@ -154,12 +154,10 @@ namespace LockedIn.Api
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-            else
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
                 app.UseHsts();
